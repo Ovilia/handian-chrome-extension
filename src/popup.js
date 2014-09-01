192fe7398a700301d2f1b4ff5f1b793b3a2eba5e
Ovilia <zwl.sjtu@gmail.com>
2014-09-01 21:21:58 +0800

    update for release

--- src/popup.js
+++ src/popup.js
@@ -4,11 +4,11 @@ var gb = {
 
 function enableToggle() {
 	gb.enabled = !gb.enabled;
-	
+
 	chrome.storage.sync.set({
 		'enabled': gb.enabled
 	}, function() {});
-	
+
 	updateHtml();
 }
 
@@ -16,7 +16,7 @@ function updateHtml() {
 	document.getElementById('enableBtn').className = gb.enabled ?
 			'enabled' : 'disabled';
 	document.getElementById('enableVal').innerHTML = gb.enabled ? '已' : '未';
-	
+
 	chrome.browserAction.setIcon({
 		path: 'handian48' + (gb.enabled ? '' : '-disabled') + '.png'
 	});
@@ -25,7 +25,7 @@ function updateHtml() {
 window.onload = function() {
 	var check = document.getElementById('enableBtn');
 	check.addEventListener('click', enableToggle, false);
-	
+
 	chrome.storage.sync.get('enabled', function(result) {
 		gb.enabled = result.enabled;
 		updateHtml();
